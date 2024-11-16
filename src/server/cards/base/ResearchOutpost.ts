ee78a1ed994ebf1cfda0b5f9ca986a4340fd288f
TechDevAlex <128148527+TechDevAlex@users.noreply.github.com>
2024-11-16 16:22:57 +0100

    created JovianResearch in ResearchOutpost

--- src/server/cards/base/ResearchOutpost.ts
+++ src/server/cards/base/ResearchOutpost.ts
@@ -1,32 +1,40 @@
 import {IProjectCard} from '../IProjectCard';
 import {Tag} from '../../../common/cards/Tag';
-import {Card} from '../Card';
 import {CardType} from '../../../common/cards/CardType';
 import {CardName} from '../../../common/cards/CardName';
 import {CardRenderer} from '../render/CardRenderer';
+import {ActionCard} from '../ActionCard';
 
-export class ResearchOutpost extends Card implements IProjectCard {
+export class ResearchOutpost extends ActionCard implements IProjectCard {
   constructor() {
     super({
       type: CardType.ACTIVE,
       name: CardName.RESEARCH_OUTPOST,
-      tags: [Tag.SCIENCE, Tag.CITY, Tag.BUILDING],
-      cost: 18,
+      tags: [Tag.JOVIAN, Tag.SCIENCE],
+      cost: 23,
 
       behavior: {
-        city: {on: 'isolated'},
+      },
+      action: {
+        stock: {megacredits: {tag: Tag.JOVIAN}},
       },
 
-      cardDiscount: {amount: 1},
+      victoryPoints: {tag: Tag.JOVIAN, per: 2},
+
+
+      requirements: {tag: Tag.JOVIAN, count: 2},
       metadata: {
-        cardNumber: '020',
+        description: {
+          text: 'Requires 2 Jovian tags to play.',
+          align: 'left',
+        },
+        cardNumber: '208',
         renderData: CardRenderer.builder((b) => {
-          b.effect('When you play a card, you pay 1 M€ less for it.', (eb) => {
-            eb.empty().startEffect.megacredits(-1);
+          b.action('Gain 1 M€ for each Jovian Tag you own.', (eb) => {
+            eb.startAction.megacredits(1).slash();
+            eb.tag(Tag.JOVIAN);
           }).br;
-          b.city();
         }),
-        description: 'Place a city tile NEXT TO NO OTHER TILE.',
       },
     });
   }
