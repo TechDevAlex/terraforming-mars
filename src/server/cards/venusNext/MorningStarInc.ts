81913f38cac4f39b362e967e549b7e3eacc1f59b
Alex <alexander.stahl.techdev@gmail.com>
2025-05-29 11:48:23 +0200

    adjusted morningstar

--- src/server/cards/venusNext/MorningStarInc.ts
+++ src/server/cards/venusNext/MorningStarInc.ts
@@ -9,19 +9,19 @@ export class MorningStarInc extends CorporationCard {
     super({
       name: CardName.MORNING_STAR_INC,
       tags: [Tag.VENUS],
-      startingMegaCredits: 38,
+      startingMegaCredits: 41,
       globalParameterRequirementBonus: {steps: 3, parameter: GlobalParameter.VENUS},
 
       firstAction: {
-        text: 'Draw 5 cards with a Venus tag',
-        drawCard: {count: 5, tag: Tag.VENUS},
+        text: 'Draw 4 cards with a Venus tag',
+        drawCard: {count: 4, tag: Tag.VENUS},
       },
 
       metadata: {
         cardNumber: 'R06',
-        description: 'You start with 38 M€. As your first action, reveal cards from the deck until you have revealed 5 Venus-tag cards. Take those into hand and discard the rest.',
+        description: 'You start with 41 M€. As your first action, reveal cards from the deck until you have revealed 4 Venus-tag cards. Take those into hand and discard the rest.',
         renderData: CardRenderer.builder((b) => {
-          b.megacredits(47).nbsp.cards(5, {secondaryTag: Tag.VENUS});
+          b.megacredits(41).nbsp.cards(4, {secondaryTag: Tag.VENUS});
           b.corpBox('effect', (ce) => {
             ce.effect('Your Venus requirements are +/- 3 steps, your choice in each case.', (eb) => {
               eb.plate('Venus requirements').startEffect.text('+/- 3');
